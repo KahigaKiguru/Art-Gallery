36b8317cf4816ed3289b341cbdb4dc29ed5d01f9
KahigaKiguru <dev.enzitechnologies@gmail.com>
2020-12-07 13:35:40 +0300

    first commit

---
+++ src/main/java/com/sindhu/ArtGallery/model/Artist.java
@@ -0,0 +1,5 @@
+package com.sindhu.ArtGallery.model;
+
+public class Artist {
+
+}
---
+++ src/test/java/com/sindhu/ArtGallery/ArtGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sindhu.ArtGallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
